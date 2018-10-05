69c31ffa599aaa844e82038eb6b2c82f1c7a7cfe
yli0101 <43018407+yli0101@users.noreply.github.com>
2018-10-04 22:18:02 -0700

    Add files via upload

---
+++ Assignment3/Question2/HappyObject.java
@@ -0,0 +1,13 @@
+package Question2;
+
+public class HappyObject extends MoodyObject{
+	protected String getMood() {
+		return "happy";
+	}
+	public void expressFeelings() {
+		System.out.println("hehehe...hahahah...HAHAHAHAHA!!!");
+	}
+	public String toString() {
+		return "Subject laughs a lot";
+	}
+}
